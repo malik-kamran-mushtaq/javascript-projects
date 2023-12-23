401c20e005fcd43b5181668a780fe93e9e128f51
malik-kamran-mushtaq <kamran145422@gmail.com>
2023-12-23 16:17:43 +0530

    DOM Basics

--- 01_DOM_Basics/script.js
+++ 01_DOM_Basics/script.js
@@ -1,6 +1,8 @@
 let bulb = document.querySelector("#bulb")
 let btn = document.querySelector("button")
 
+let box = document.querySelector("#box")
+
 let flag = 0;
 btn.addEventListener("click", () => {
     if(flag == 0) {
@@ -12,3 +14,13 @@ btn.addEventListener("click", () => {
     }
     
 })
+
+// h1
+let h = document.querySelectorAll("h1")
+h.forEach((e) => {      // forEach on the NodeList
+    console.log(e);
+})
+
+//+++++++++++
+box.innerHTML = "<h1>Lets gooo</h1>"
+// box.textContent = "<h1>Lets gooo</h1>"
